76afaacb4db6c3bab5935ed40dee867f585d95a6
Khadija1400 <khadijaa.1400@gmail.com>
2024-08-04 21:27:29 +0300

    new

---
+++ MyPlayground.swich.playground/Contents.swift
@@ -0,0 +1,26 @@
+import UIKit
+
+let grade = 72
+
+switch grade {
+    
+    case 90...100 :
+        
+        print ("Excellent")
+        
+    case 80...89 :
+        
+        print ("Very Good")
+        
+    case 70...79 :
+        
+        print ("Good")
+    case 60...69 :
+        
+        print ("Pass")
+
+    default:
+        
+        print ("Fail")
+
+}
